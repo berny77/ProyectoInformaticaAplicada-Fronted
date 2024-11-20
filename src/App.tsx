8a54cb3b19bbd7b79f9a63f89b6ed43184d792b3
Jafet <correo@example.com>
2024-11-19 18:02:03 -0600

    body style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar1 from './components/Navbar';
-/* import Navbar from './components/NavBar'; */
+import Navbar1 from './components/NavBar';
 import Login from './components/Login';
 import Register from './components/Register';
 import FileGrid from './components/File/FileGrid'; // Cambiado a FileGrid
